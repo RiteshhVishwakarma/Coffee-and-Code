777c993545e97da9bce7f22125979a84f9b2cb7d
Ritesh Vishwakarma <113338495+RiteshhVishwakarma@users.noreply.github.com>
2025-04-13 12:03:27 +0530

    update setting

--- health_and_fitness_tracker/urls.py
+++ health_and_fitness_tracker/urls.py
@@ -3,13 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("tracker.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 # Static files serve karne ke liye (only for local testing when DEBUG=False)
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if not settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
 
 # http://127.0.0.1:8000/staticfiles/css/index.css
